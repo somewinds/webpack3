9b332a98ea05892adb71ecadaaabce998e4a70ae
liuyajun <liuyajun@linhuiba.com>
2019-02-27 11:10:46 +0800

    查看first.js和second.js，会发现共同引用的common.js文件和jquery都被打包进去了，这肯定不合理，公共模块重复打包，体积过大。
    分离出第三方库、自定义公共模块、webpack运行文件

--- CommonsChunkPlugin/webpack.config.js
+++ CommonsChunkPlugin/webpack.config.js
@@ -1,15 +1,23 @@
 const path = require("path");
 const webpack = require("webpack");
+const packagejson = require("./package.json");
 
 const config = {
   entry: {
     first: "./src/first.js",
     second: "./src/second.js",
+    vendor: Object.keys(packagejson.dependencies) // 获取生产环境依赖的库
   },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].js'
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: '[name].js'
+    })
+  ]
 }
 
 module.exports = config;
